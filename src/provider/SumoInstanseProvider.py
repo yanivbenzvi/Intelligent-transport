8f18945cd0b41a077049c5ad6449a0baa06ff053
Yaniv Ben Zvi <yanivbenzvi023@gmail.com>
2020-04-21 18:51:51 +0300

    add SumoInsanceProvider - sumo insances boot

---
+++ src/provider/SumoInstanseProvider.py
@@ -0,0 +1,15 @@
+class SumoInstanseProvider:
+    def __init__(self, senario):
+        self.xmlFile = senario
+
+    def boot(self):
+
+
+    @staticmethod
+    def instance_list():
+        return {
+            'lanes': {
+                'ctor' :
+            }
+        }
+
