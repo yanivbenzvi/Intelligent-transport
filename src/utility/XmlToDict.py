296f5d6c5e50a66ec80e5763f19f8610859ca27a
Yaniv Ben Zvi <yanivbenzvi023@gmail.com>
2020-06-15 10:02:03 +0300

    set XmlToDict code to function

--- src/utility/XmlToDict.py
+++ src/utility/XmlToDict.py
@@ -4,12 +4,17 @@ import json
 import os
 from src.Configuration import project_path
 
-with open(os.path.join(project_path, "simulator/MoSTScenario-0.6/scenario/in/most.net.xml")) as fd:
-    doc = xmltodict.parse(fd.read())
 
-pp = pprint.PrettyPrinter(indent=4)
-j = json.dumps(doc, indent=4, sort_keys=True)
-with open("data.json", 'w') as file:
-    file.write(j)
+def xml2dict(path):
+    with open(os.path.join(project_path, path)) as fd:
+        doc = xmltodict.parse(fd.read())
+    return doc
+    # pp = pprint.PrettyPrinter(indent=4)
+    # j = json.dumps(doc, indent=4, sort_keys=True)
+    # with open("../../tests/resources/data.json", 'w') as file:
+    #     file.write(j
 
-print(doc['net']['tlLogic'])
+
+if __name__ == "__main__":
+    docs = xml2dict("simulator/MoSTScenario-0.6/scenario/in/most.net.xml")
+    print(docs['net']['tlLogic'])
