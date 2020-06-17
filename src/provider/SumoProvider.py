9bbf722d842e2561946d32e9340bffd779da20de
Yaniv Ben Zvi <yanivbenzvi023@gmail.com>
2020-06-17 14:01:41 +0300

    integrate store and subscribe class with SumoProvider.py

--- src/provider/SumoProvider.py
+++ src/provider/SumoProvider.py
@@ -1,4 +1,6 @@
 from src.provider.SumoInstanceProvider import SumoInstanceProvider
+from src.utility.Store import Store
+from src.utility.AllSubscribe import AllSubscribe
 from src import Configuration
 from collections import deque
 import traci.constants as tc
@@ -12,6 +14,8 @@ class SumoProvider:
     def __init__(self):
         self.number_of_iteration = 300000
         self.simulator_delay = 0
+        self.store = None
+        self.traci = traci
 
     def boot(self):
         if 'SUMO_HOME' in os.environ:
@@ -20,24 +24,37 @@ class SumoProvider:
         else:
             sys.exit("please declare environment variable 'SUMO_HOME'")
 
+        # set simulator choice from the list of the scenario
         SumoProvider.prefer_simulation()
-        Instance_provider = SumoInstanceProvider(Configuration.scenario_object)
-        Instance_provider.boot()
-        Configuration.scenario_object.traci = traci
-        traci.start(Configuration.sumoCmd())
-        print(Configuration.scenario_object.scenario_duration())
+
+        # import all nessacceary object from the simulator
+        instance_provider = SumoInstanceProvider(Configuration.scenario_object)
+
+        # create store of all instances
+        self.store = Store(instance_provider.boot().all_instance)
+
+        # subscribe all relevant instance for future update
+        AllSubscribe(self.traci).subscribes_all_instance()
+
+        # make traci to be global
+        Configuration.scenario_object.traci = self.traci
+
+        # start the sumo simulation program
+        self.traci.start(Configuration.sumoCmd())
+
+        # print(Configuration.scenario_object.scenario_duration())
         # print(Configuration.sumoCmd())
 
         # make subscribe to all instance
-        traci.lane.subscribe("152780_1", {traci.constants.LAST_STEP_OCCUPANCY})
+        # traci.lane.subscribe("152780_1", {traci.constants.LAST_STEP_OCCUPANCY})
         # traci.vehicle.subscribe("pedestrian_1-3_2239_tr")
-        traci.lane.subscribe("152780_1")
+        # traci.lane.subscribe("152780_1")
         # traci.lane.subscribeLeader(vehID="EXT", dist=0)
 
-        for step in range(self.number_of_iteration):
+        while self.traci.simulation.getMinExpectedNumber() > 0:
             try:
-                traci.simulationStep()
-            except traci.exceptions.FatalTraCIError as inst:
+                self.traci.simulationStep()
+            except self.traci.exceptions.FatalTraCIError as inst:
                 print(inst)
                 break
             # print(traci.simulation.getCurrentTime(), traci.lane.getAllSubscriptionResults())
@@ -49,7 +66,7 @@ class SumoProvider:
             time.sleep(self.simulator_delay)
             # print(step)
 
-        traci.close()
+        self.traci.close()
 
     @staticmethod
     def prefer_simulation():
